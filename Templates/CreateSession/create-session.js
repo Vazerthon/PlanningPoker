91996aed5fc853e004cb271169aa5cd41c650e74
Paul Vallis <Paul.Vallis@G2G3DTA279921.G2G3.net>
2015-09-07 15:40:39 +0100

    Split session creation into it's own template

---
+++ Templates/CreateSession/create-session.js
@@ -0,0 +1,26 @@
+﻿if (Meteor.isClient) {
+    Meteor.subscribe(sessionsCollectionName);
+
+    Template.createSession.events({
+        'submit .new-session' : function () {
+            event.preventDefault();
+            var newSessionName = event.target.sessionName.value || 'unnamed session';
+            Meteor.call('createSession', newSessionName);
+            event.target.sessionName.value = '';
+        }
+    });
+}
+
+Meteor.methods({
+    createSession: function(sessionName) {
+        if (!Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
+
+        Sessions.insert({
+            Name: sessionName,
+            Owner: Meteor.user(),
+            Created: new Date()
+        });
+    }
+});
--- Templates/Sessions/sessions.js
+++ Templates/Sessions/sessions.js
@@ -11,28 +11,8 @@
     });
 
     Template.sessions.events({
-        'submit .new-session' : function () {
-            event.preventDefault();
-            var newSessionName = event.target.sessionName.value || 'unnamed session';
-            Meteor.call('createSession', newSessionName);
-            event.target.sessionName.value = '';
-        },
         'change #sessionPicker': function () {
             Session.set(selectedSession, Sessions.findOne(event.target.value));
         }
     });
 }
-
-Meteor.methods({
-    createSession: function(sessionName) {
-        if (!Meteor.userId()) {
-            throw new Meteor.Error("not-authorized");
-        }
-
-        Sessions.insert({
-            Name: sessionName,
-            Owner: Meteor.user(),
-            Created: new Date()
-        });
-    }
-});
